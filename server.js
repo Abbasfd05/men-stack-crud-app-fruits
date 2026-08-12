970fe5318e37574aef39e14a2a4db5ad8e67ed60
Abbasfd05 <abbasfadhel2005abfd@gmail.com>
2026-08-12 15:26:58 +0300

    updated

--- server.js
+++ server.js
@@ -7,10 +7,11 @@ const express = require('express');
 const mongoose = require("mongoose"); // require package
 const app = express();
 const morgan=require('morgan');
-// middleware
-app.use(express.urlencoded({extended:false}));
-app.use(morgan("dev"));
-
+const methodOverride=require("method-override");
+// middleware: always app.use method, and just tell the method-override what query to use default: method
+app.use(express.urlencoded({extended:false})); // change the request
+app.use(morgan("dev")); //log the request
+app.use(methodOverride("_method"));
 // Connect to MongoDB using the connection string in the .env file
 mongoose.connect(process.env.MONGODB_URI);
 // log connection status to terminal on start
@@ -59,6 +60,34 @@ res.render("fruits/show.ejs", { fruit});
 } catch(error) {
     console.log("The error is " , error);
 }
+});
+app.delete("/fruits/:id" , async (req,res) => { //delete doesn't conflict with get so we will use the same URL
+    try {
+  await Fruit.findByIdAndDelete(req.params.id);
+    res.redirect("/fruits");
+
+} catch(error) {
+    console.log("The error is " , error);
+}
+});
+
+app.put("/fruits/:fruitId", async (req, res) => {
+  // Handle the 'isReadyToEat' checkbox data
+  if (req.body.isReadyToEat === "on") {
+    req.body.isReadyToEat = true;
+  } else {
+    req.body.isReadyToEat = false;
+  }
+  
+  // Update the fruit in the database
+  try {
+  await Fruit.findByIdAndUpdate(req.params.fruitId, req.body);
+
+  // Redirect to the fruit's show page to see the updates
+  res.redirect(`/fruits/${req.params.fruitId}`);
+  } catch(err) {
+    console.log("The error is " + err);
+  }
 });
 app.listen(3000, () => {
   console.log('Listening on port 3000');
