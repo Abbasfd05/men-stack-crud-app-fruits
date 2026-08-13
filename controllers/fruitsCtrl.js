a92a4c8476fb5b5a9875d6a38732004ef85fca75
Abbasfd05 <abbasfadhel2005abfd@gmail.com>
2026-08-13 14:06:00 +0300

    MVC architecture

---
+++ controllers/fruitsCtrl.js
@@ -0,0 +1,84 @@
+// controllers/fruits.js
+
+const Fruit = require('../models/fruit');
+
+const home = (req, res) => {
+  res.render('index.ejs');
+};
+
+const showNewForm = (req, res) => {
+  res.render('fruits/new.ejs');
+};
+
+const create = async (req, res) => {
+  try {
+    req.body.isReadyToEat = req.body.isReadyToEat === 'on';
+    await Fruit.create(req.body);
+    res.redirect('/fruits');
+  } catch (err) {
+    console.log(err);
+    res.send('failed to create');
+  }
+};
+
+const index = async (req, res) => {
+  try {
+    const foundFruits = await Fruit.find();
+    res.render('fruits/index.ejs', { fruits: foundFruits });
+  } catch (err) {
+    console.log(err);
+    res.send('failed to get all fruits');
+  }
+};
+
+const show = async (req, res) => {
+  try {
+    const fruit = await Fruit.findById(req.params.id);
+    res.render('fruits/show.ejs', { fruit });
+  } catch (err) {
+    console.log(err);
+    res.send('failed to fetch the fruit');
+  }
+};
+
+const deleteFruit = async (req, res) => {
+  try {
+    await Fruit.findByIdAndDelete(req.params.id);
+    res.redirect('/fruits');
+  } catch (err) {
+    console.log(err);
+    res.send('unable to delete fruit');
+  }
+};
+
+const editForm = async (req, res) => {
+  try {
+    const fruit = await Fruit.findById(req.params.id);
+    res.render('fruits/edit.ejs', { fruit });
+  } catch (err) {
+    console.log(err);
+    res.send('unable to load edit form');
+  }
+};
+
+const update = async (req, res) => {
+  try {
+    req.body.isReadyToEat = req.body.isReadyToEat === 'on';
+    await Fruit.findByIdAndUpdate(req.params.id, req.body);
+    res.redirect(`/fruits/${req.params.id}`);
+  } catch (err) {
+    console.log(err);
+    res.send('unable to update the fruit');
+  }
+};
+
+module.exports = {
+  home,
+  showNewForm,
+  create,
+  index,
+  show,
+  deleteFruit,
+  editForm,
+  update,
+};
--- server.js
+++ server.js
@@ -4,11 +4,11 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
-const path = require("path");
+const path = require('path');
 const methodOverride = require('method-override');
 
-// Models
-const Fruit = require('./models/fruit');
+// Controllers
+const fruitsController = require('./controllers/fruits');
 
 const app = express();
 
@@ -16,95 +16,25 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
+
 // MONGO DB CONNECTION
 mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
-app.get('/', (req, res) => {
-  res.render('index.ejs');
-});
+// ROUTES
+app.get('/', fruitsController.home);
 
 // Fruits
-app.get('/fruits/new', (req, res) => {
-  res.render('fruits/new.ejs');
-});
-
-app.post('/fruits', async (req, res) => {
-  try {
-    if (req.body.isReadyToEat === 'on') {
-      req.body.isReadyToEat = true;
-    } else {
-      req.body.isReadyToEat = false;
-    }
-
-    await Fruit.create(req.body);
-
-    res.redirect('/fruits');
-  } catch (err) {
-    console.log(err);
-    res.send('failed to create');
-  }
-});
-
-app.get('/fruits', async (req, res) => {
-  try {
-    const fruits = await Fruit.find();
-    res.render('fruits/index.ejs', { fruits });
-  } catch (err) {
-    console.log(err);
-    res.send('failed to get all fruits');
-  }
-});
-
-app.get('/fruits/:id', async (req, res) => {
-  try {
-    const fruit = await Fruit.findById(req.params.id);
-
-    res.render('fruits/show.ejs', { fruit });
-  } catch (err) {
-    console.log(err);
-    res.send('failed to fetch the fruit');
-  }
-});
-
-app.delete('/fruits/:id', async (req, res) => {
-  try {
-    await Fruit.findByIdAndDelete(req.params.id);
-    res.redirect('/fruits');
-  } catch (err) {
-    console.log(err);
-    res.send('unable to delete fruit');
-  }
-});
-
-app.get('/fruits/:id/edit', async (req, res) => {
-  try {
-    const fruit = await Fruit.findById(req.params.id);
-    res.render('fruits/edit.ejs', { fruit });
-  } catch (err) {
-    console.log(err);
-    res.send('unable to update the fruit');
-  }
-});
-
-app.put('/fruits/:id', async (req, res) => {
-  try {
-    if (req.body.isReadyToEat === 'on') {
-      req.body.isReadyToEat = true;
-    } else {
-      req.body.isReadyToEat = false;
-    }
-
-    await Fruit.findByIdAndUpdate(req.params.id, req.body);
-    res.redirect(`/fruits/${req.params.id}`);
-  } catch (err) {
-    console.log(err);
-    res.send('unable to update the fruit');
-  }
-});
+app.get('/fruits/new', fruitsController.showNewForm);
+app.post('/fruits', fruitsController.create);
+app.get('/fruits', fruitsController.index);
+app.get('/fruits/:id', fruitsController.show);
+app.delete('/fruits/:id', fruitsController.deleteFruit);
+app.get('/fruits/:id/edit', fruitsController.editForm);
+app.put('/fruits/:id', fruitsController.update);
 
 app.listen(3000, () => {
   console.log('server is running!!!!');
